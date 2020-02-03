bd50dddfebfc3d97f9ea0307b9e3b29a2df940d0
David Hoffman <dave.p.hoffman@gmail.com>
2020-02-03 14:23:40 -0800

    Update day04.py

--- day04.py
+++ day04.py
@@ -30,6 +30,27 @@ def monotonic(key):
     return True
 
 
+def double2(key):
+    """check if there are any doubles that are isolated"""
+    # make a list of the key in the right order
+    key = list(str(key))[::-1]
+    length = 1
+    k0 = key.pop()
+    # cycle through keys
+    while key:
+        k1 = key.pop()
+        if k0 == k1:
+            length += 1
+        else:
+            if length == 2:
+                return True
+            length = 1
+        k0 = k1
+    if length == 2:
+        return True
+    return False
+
+
 def test():
     for key, truth in test_inputs:
         assert double(key) and monotonic(key) == truth
@@ -37,8 +58,9 @@ def test():
 
 def main():
     valid_keys = filter(double, range(372037, 905157 + 1))
-    valid_keys = filter(monotonic, valid_keys)
-    print(len(list(valid_keys)))
+    valid_keys = tuple(filter(monotonic, valid_keys))
+    print("Answer 1:", len(valid_keys))
+    print("Answer 2:", len(tuple(filter(double2, valid_keys))))
 
 
 if __name__ == "__main__":
